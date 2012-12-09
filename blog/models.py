c75f5f4de1b7617a7ccdda9df1b2bab72b80d2e2
Aaron White <wyrmwood@gmail.com>
2012-12-09 09:50:15 -0500

    remove unwanted files

--- blog/models.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import db
-
-class BaseModel(db.Model):
-  created = db.DateTimeProperty(auto_now_add=True)
-
-class Entry(BaseModel):
-  title = db.StringProperty(default="")
-  body = db.TextProperty(default="")
